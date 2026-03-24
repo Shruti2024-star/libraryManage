c4d2e86d7a1cc60e7b42554ac95119a7e1a8db5a
Polisetti Shruti <polishettyshruti@gmail.com.com>
2026-03-24 23:09:18 +0530

    update security config

--- project1/src/main/java/com/libraryManagement/project1/security/SecurityConfig.java
+++ project1/src/main/java/com/libraryManagement/project1/security/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         // Remove the trailing slash
-        configuration.setAllowedOrigins(List.of("https://library-frontend-zeta-nine.vercel.app/"));
+        configuration.setAllowedOrigins(List.of("https://library-frontend-zeta-nine.vercel.app"));
  
         
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
